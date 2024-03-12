d06b9368d1f2a664df7f68b3b675cf6a7768588b
HelyssonBN3040 <helyssoncavalcante.com.@gmail.com>
2024-03-12 14:57:32 -0300

    second commit

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,24 +1,27 @@
 import React from 'react'
+import "./HomePage.css"
 import { Link } from 'react-router-dom'
 
 const HomePage = () => {
   return (
-    <div>
-      <h1 className="text-3xl">Home Page</h1>
-      <div className="container bg-gray-100 p-4">
+    <>
+      <div className="container">
         <div className="container-header">
-          <h1>Welcome to our website!</h1>
+          <h1>Seja Bem-Vindo ao Meu Portfolio!</h1>
         </div>
         <div className="list-projects">
           <ul>
-            <li className="options"><Link to="/projects">Projects</Link></li>
-            <li className="options"><Link to="/about">Sobre Mim</Link></li>
-            <li className="options"><Link to="/contact">Contatos</Link></li>
-            <li className="options"><Link to="/certificates">Certificados</Link></li>
+            <li><Link className="options" to="/projects">Projects</Link></li>
+            <li><Link className="options" to="/about">Sobre Mim</Link></li>
+            <li><Link className="options" to="/contact">Contatos</Link></li>
+            <li><Link className="options" to="/certificates">Certificados</Link></li>
           </ul>
         </div>
+        <div className="tecnologies">
+          
+        </div>
       </div>
-    </div>
+    </>
   )
 }
 
